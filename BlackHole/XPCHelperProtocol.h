d18ed1a86dff4a701ddfa625f177105ccb8aa821
steven <wc@gaudiolab.com>
2022-11-30 13:15:15 +0900

    JJOC 2ch

---
+++ BlackHole/XPCHelperProtocol.h
@@ -0,0 +1,41 @@
+//
+//  XPCHelperProtocol.h
+//  XPCHelper
+//
+//  Created by joey on 2022/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+// The protocol that this service will vend as its API. This header file will
+// also need to be visible to the process hosting the service.
+@protocol XPCHelperProtocol
+- (void) upperCaseString:(NSString *)aString withReply:(void (^)(NSString *))reply;
+
+- (void) registerAsAppWithListenerEndpoint:(NSXPCListenerEndpoint*)endpoint reply:(void (^)(void))reply;
+- (void) unregisterAsApp;
+- (void) connectWithProcessIdToApp:(NSUInteger)processId ioType:(bool)isInput withReply:(void (^)(NSError*))reply;
+@end
+
+/*
+ To use the service from an application or other process, use NSXPCConnection to
+establish a connection to the service by doing something like this:
+
+     _connectionToService = [[NSXPCConnection alloc]
+initWithServiceName:@"com.gaudiolab.XPCHelper"];
+     _connectionToService.remoteObjectInterface = [NSXPCInterface
+interfaceWithProtocol:@protocol(XPCHelperProtocol)];
+     [_connectionToService resume];
+
+Once you have a connection to the service, you can use it like this:
+
+     [[_connectionToService remoteObjectProxy] upperCaseString:@"hello"
+withReply:^(NSString *aString) {
+         // We have received a response. Update our text field, but do it on the
+main thread. NSLog(@"Result string was: %@", aString);
+     }];
+
+ And, when you are finished with the service, clean up the connection like this:
+
+     [_connectionToService invalidate];
+*/
--- Installer/create_installer.sh
+++ Installer/create_installer.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env zsh
 
-
-
-# create installer for different channel versions
-
-for channels in 2 16 64 128 256
-do
-
+# channels in 2 16 64 128 256
+channels=2
+# channels=16
 ch=$channels"ch"
-version=v$(head -n 1 VERSION)
-bundleID="audio.existential.BlackHole$ch"
+driverName="JJOC"
+bundleID="gaudio.driver.JJOC$ch"
+icon="JJOC.icns"
 
 # Build
 xcodebuild \
@@ -17,60 +14,15 @@ xcodebuild \
 -configuration Release \
 -target BlackHole CONFIGURATION_BUILD_DIR=build \
 PRODUCT_BUNDLE_IDENTIFIER=$bundleID \
-GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS kNumber_Of_Channels='$channels' kPlugIn_BundleID=\"'$bundleID'\"'
+GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS kNumber_Of_Channels='$channels'
+kDriver_Name=\"'$driverName'\"
+kPlugIn_BundleID=\"'$bundleID'\"
+kPlugIn_Icon=\"'$icon'\"'
 
+rm -rf installer/root
 mkdir installer/root
-mv build/BlackHole.driver installer/root/BlackHole$ch.driver
+mv build/JJOC.driver installer/root/JJOC$ch.driver
 rm -r build
 
 # Sign
-codesign --force --deep --options runtime --sign Q5C99V536K Installer/root/BlackHole$ch.driver
-
-# Create package with pkgbuild
-chmod 755 Installer/Scripts/preinstall
-chmod 755 Installer/Scripts/postinstall
-
-pkgbuild --sign "Q5C99V536K" --root Installer/root --scripts Installer/Scripts --install-location /Library/Audio/Plug-Ins/HAL Installer/BlackHole.pkg
-rm -r Installer/root
-
-# Create installer with productbuild
-cd Installer
-
-echo "<?xml version=\"1.0\" encoding='utf-8'?>
-<installer-gui-script minSpecVersion='2'>
-    <title>BlackHole: Virtual Audio Driver $ch $version</title>
-    <welcome file='welcome.html'/>
-    <license file='../LICENSE'/>
-    <conclusion file='conclusion.html'/>
-    <domains enable_anywhere='false' enable_currentUserHome='false' enable_localSystem='true'/>
-    <pkg-ref id=\"$bundleID\"/>
-    <options customize='never' require-scripts='false' hostArchitectures='x86_64,arm64'/>
-    <volume-check>
-        <allowed-os-versions>
-            <os-version min='10.9'/>
-        </allowed-os-versions>
-    </volume-check>
-    <choices-outline>
-        <line choice=\"audio.existential.BlackHole$ch\"/>
-    </choices-outline>
-    <choice id=\"audio.existential.BlackHole$ch\" visible='true' title=\"BlackHole $ch\" start_selected='true'>
-        <pkg-ref id=\"audio.existential.BlackHole$ch\"/>
-    </choice>
-    <pkg-ref id=\"audio.existential.BlackHole$ch\" version=\"$version\" onConclusion='none'>BlackHole.pkg</pkg-ref>
-</installer-gui-script>" >> distribution.xml
-
-
-productbuild --sign "Q5C99V536K" --distribution distribution.xml --resources . --package-path BlackHole.pkg BlackHole$ch.$version.pkg
-rm distribution.xml
-rm -f BlackHole.pkg
-
-# Notarize
-xcrun notarytool submit BlackHole$ch.$version.pkg --team-id Q5C99V536K --progress --wait --keychain-profile "Notarize"
-
-xcrun stapler staple BlackHole$ch.$version.pkg
-
-cd ..
-
-done
-
-
+codesign --force --deep --options runtime --sign S4VKPVFH2D Installer/root/JJOC$ch.driver
