cf687b4dc07be5e80611db6aacc48a5e5b67b227
Jack <n@gaudiolab.com>
2022-12-08 19:35:54 +0900

    update: icns, remove name %ich

--- Installer/create_installer.sh
+++ Installer/create_installer.sh
@@ -3,23 +3,20 @@
 rm -rf installer/root
 mkdir installer/root
 
+driverName="JJOC"
+icon="JJOC.icns"
+sampleRates=48000
+
 for driverType in "MIC" "SPK"
 do
-    if [ driverType = "MIC" ]
+    if [ ${driverType} = "MIC" ];
     then
         channels=1
-        # hasInput=1
-        # hasOutput=0
     else
-        channels=1
-        # hasInput=0
-        # hasOutput=1
+        channels=2
     fi
 
-    ch=${channels}"ch"
-    driverName="JJOC-${driverType}"
-    bundleID="gaudio.driver.${driverName}${ch}"
-    icon="JJOC.icns"
+    bundleID="gaudio.driver.${driverName}${driverType}"
 
     # Build
     xcodebuild \
@@ -29,12 +26,14 @@ do
     PRODUCT_BUNDLE_IDENTIFIER=$bundleID \
     GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS kNumber_Of_Channels='$channels'
     kDriver_Name=\"'$driverName'\"
+    kDriver_Type=\"'$driverType'\"
     kPlugIn_BundleID=\"'$bundleID'\"
+    kSampleRates='$sampleRates'
     kPlugIn_Icon=\"'$icon'\"'
 
-    mv build/JJOC.driver installer/root/${driverName}${ch}.driver
+    mv build/JJOC.driver installer/root/${driverName}${driverType}.driver
     rm -r build
 
     # Sign
-    codesign --force --deep --options runtime --sign S4VKPVFH2D Installer/root/${driverName}${ch}.driver
+    codesign --force --deep --options runtime --sign S4VKPVFH2D Installer/root/${driverName}${driverType}.driver
 done
