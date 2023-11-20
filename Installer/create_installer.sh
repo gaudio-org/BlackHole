07a9a5e3673dc9428235f3b1712bb044d02b7351
Jack Noh <n@gaudiolab.com>
2023-11-20 17:32:48 +0900

    fix: re-naming & rm XPC

--- Installer/create_installer.sh
+++ Installer/create_installer.sh
@@ -5,42 +5,31 @@ mkdir installer/root
 
 driverName="JustVoice"
 icon="JustVoice.icns"
+channels=2
 
-for driverType in "SPK"
-do
-    if [ ${driverType} = "MIC" ];
-    then
-        channels=1
-    else
-        channels=2
-    fi
+bundleID="gaudio.driver.${driverName}"
 
-    bundleID="gaudio.driver.${driverName}${driverType}"
+# Build
+xcodebuild \
+-project BlackHole.xcodeproj \
+-configuration Release \
+-target BlackHole CONFIGURATION_BUILD_DIR=build \
+PRODUCT_BUNDLE_IDENTIFIER=$bundleID \
+GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS kNumber_Of_Channels='$channels'
+kDriver_Name=\"'$driverName'\"
+kPlugIn_BundleID=\"'$bundleID'\"
+kPlugIn_Icon=\"'$icon'\"'
 
-    # Build
-    xcodebuild \
-    -project BlackHole.xcodeproj \
-    -configuration Release \
-    -target BlackHole CONFIGURATION_BUILD_DIR=build \
-    PRODUCT_BUNDLE_IDENTIFIER=$bundleID \
-    GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS kNumber_Of_Channels='$channels'
-    kDriver_Name=\"'$driverName'\"
-    kDriver_Type=\"'$driverType'\"
-    kPlugIn_BundleID=\"'$bundleID'\"
-    kPlugIn_Icon=\"'$icon'\"'
+mv build/JustVoice.driver installer/root/${driverName}.driver
+rm -r build
 
-    mv build/JustVoice.driver installer/root/${driverName}${driverType}.driver
-    rm -r build
+# Sign
+codesign --force --deep --options runtime --sign 'Developer ID Application: Gaudio Lab (BPQH2TQNZ4)' Installer/root/${driverName}.driver
 
-    # Sign
-    codesign --force --deep --options runtime --sign 'Developer ID Application: Gaudio Lab (BPQH2TQNZ4)' Installer/root/${driverName}${driverType}.driver
+# TODO:zip
 
-    # TODO:zip
+# notarization
+# xcrun altool --notarize-app --primary-bundle-id "gaudio.driver.JustVoice" --username "info@gaudiolab.com" --password "iego-linv-bbgk-azmn" --asc-provider "BPQH2TQNZ4" --file Installer/root/JustVoice.driver.zip
 
-    # notarization
-    # xcrun altool --notarize-app --primary-bundle-id "gaudio.driver.JustVoiceMIC" --username "info@gaudiolab.com" --password "iego-linv-bbgk-azmn" --asc-provider "BPQH2TQNZ4" --file Installer/root/JustVoiceMIC.driver.zip
-    # xcrun altool --notarize-app --primary-bundle-id "gaudio.driver.JustVoiceSPK" --username "info@gaudiolab.com" --password "iego-linv-bbgk-azmn" --asc-provider "BPQH2TQNZ4" --file Installer/root/JustVoiceSPK.driver.zip
-
-    # check notarization
-    # xcrun altool --notarization-history 0 -u "info@gaudiolab.com" -p "iego-linv-bbgk-azmn"
-done
+# check notarization
+# xcrun altool --notarization-history 0 -u "info@gaudiolab.com" -p "iego-linv-bbgk-azmn"
